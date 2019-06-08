f8e77f044d681f62902894cc33f0b776049ddbd4
abidhussain <abid786@gmail.com>
2019-06-08 16:28:49 +0300

    First Commit

---
+++ src/main/resources/db/sql/insert-data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (name, email, framework) VALUES ('abid', 'abid@gmail.com', 'Spring MVC, GWT');
+INSERT INTO users (name, email, framework) VALUES ('martin', 'martin@yahoo.com', 'Spring MVC, PLAY');
+INSERT INTO users (name, email, framework) VALUES ('naeem', 'naeem@gmail.com', 'Spring MVC, JSF 2');
